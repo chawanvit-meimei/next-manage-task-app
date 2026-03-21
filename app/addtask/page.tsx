008435d05a835124bebee5a63f4151396fdbec43
chawanvit-meimei <s6619n10006@sau.ac.th>
2026-03-21 12:00:58 +0700

    complete v0.1.0

---
+++ app/addtask/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div className="w-2/4 mx-auto flex flex-col items-center border-gray-500 rounded-lg shadow-xl p-10">
+            <h2 className="text-xl font-semibold mb-4">Add New Task</h2>
+            {/* TODO: add form inputs here */}
+        </div>
+    );
+}
---
+++ components/FooterSau.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+    return (
+        <>
+            <hr className="my-5 mx-auto w-3/4"/>
+            <p className="text-center mb-10">
+                Copyright @2026 All rights reserved.
+                <br />
+                created by chawanvit S. S6619N10006
+            </p>
+        </>
+    );
+}
